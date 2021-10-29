356ac3043f810d0d8ce2995f1ccc941e02737d8d
Machine Herald <canthandlethetruth01@gmail.com>
2021-10-29 16:14:23 -0400

    adds close event handler

--- src/sys/handler/Clients.js
+++ src/sys/handler/Clients.js
@@ -1,4 +1,5 @@
 import Message_Handler from "../handlers/Message_Handler"
+import Socket_Close from "./Error"
 import Channel_Messages from "./Channel_Mesages"
 import Ping from "./Ping"
 
@@ -13,34 +14,35 @@ class Clients {
           [Channel_Messages.EVENT]: new Channel_Messages(ctx.wss),
           [Ping.EVENT]: new Ping()
         })
+        this.close_handler = new Socket_Close(ctx)
     }
 
     init_handlers(client) {
         console.log(`red pill [${client.id}] has joined the Nebuchadnezzar!`)
+        
         client.on('message', (msg) => {
-          const payload = JSON.parse(msg)
-          console.log(`Red Pill [${client.id}][payload]: `, payload)
-
-
-          this.message_handler.handle({
-              ...payload,
-              open_state: this.WebSocket.OPEN,
-              ws: client
-          })
+            const payload = JSON.parse(msg)
+            console.log(`Red Pill [${client.id}][payload]: `, payload)
+            this.message_handler.handle({
+                ...payload,
+                open_state: this.WebSocket.OPEN,
+                ws: client
+            })
         })
         
         client.on('close', (code, reason) => {
-          console.log(`[RedPill][${client.id}] left the matrix...`)
-          console.log(`[code]: ${code}`)
-          console.log(`[reason]: ${reason}`)
+            this.close_handler.handle({
+                code: code,
+                reason: reason
+            }, client)
         })
     }
 
     create_client(ws, req) {
         const id = req.url.replace('/?client=', '')
-        ws.id = id
 
         this.clients[id] = ws
+        this.clients[id]['id'] = id
         this.init_handlers(this.clients[id])
     }
 }
--- src/sys/handlers/Message_Handler.js
+++ src/sys/handlers/Message_Handler.js
@@ -1,15 +1,15 @@
 class Message_Handler {
   constructor(handlers) {
-    this.handlers = handlers
-    this.handle = this.handle.bind(this)
+      this.handlers = handlers
+      this.handle = this.handle.bind(this)
   }
 
   handle(msg) {
-    if (!this.handlers[msg.event]) {
-        throw new Error('No handler for message')
-    }
+      if (!this.handlers[msg.event]) {
+          throw new Error('No handler for message')
+      }
 
-    this.handlers[msg.event].handle(msg)
+      this.handlers[msg.event].handle(msg)
   }
 }
 
