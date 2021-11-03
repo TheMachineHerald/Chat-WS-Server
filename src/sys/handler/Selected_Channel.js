90c31132a621b4d9cc88f06f6de1452f6559bb94
Machine Herald <canthandlethetruth01@gmail.com>
2021-11-02 23:08:27 -0400

    adds server socket functionality

--- src/sys/handler/Selected_Channel.js
+++ src/sys/handler/Selected_Channel.js
@@ -2,37 +2,20 @@ class Selected_Channel {
 	constructor(wss) {
 		this.wss = wss
 		this.hydrate = this.hydrate.bind(this)
-		// this.broadcast = this.broadcast.bind(this)
 		this.handle = this.handle.bind(this)
 	}
 
 	hydrate(msg) {
+		// const message = {
+		// 	event: "UPDATE_SELECTED_CHANNEL",
+		// 	payload: {}
+		// }
 		msg.ws.selected_channel_id = msg.payload.selected_channel_id
 		console.log("client: ", msg.ws.id, msg.payload.selected_channel_id)
 	}
 
-	// broadcast(msg) {
-	// 	const message = {
-	// 		event: "UPDATE_SELECTED_CHANNEL",
-	// 		payload: {}
-	// 	}
-	// 	this.wss.clients.forEach(client => {
-	// 		if (client.readyState === msg.open_state) {
-	// 			client.cache.forEach(s => {
-	// 				if (
-	// 					s.id === msg.payload.id
-	// 				) {
-	// 					console.log("[NEBUCHADNEZZAR]: user match: ", msg.payload)
-	// 					client.send(JSON.stringify(message))
-	// 				}
-	// 			})
-	// 		}
-	// 	})
-	// }
-
 	handle(msg) {
 		this.hydrate(msg)
-		// this.broadcast(msg)
 	}
 }
 
--- src/sys/handler/Selected_Server.js
+++ src/sys/handler/Selected_Server.js
@@ -2,22 +2,20 @@ class Selected_Server {
 	constructor(wss) {
 		this.wss = wss
 		this.hydrate = this.hydrate.bind(this)
-		// this.handle = this.handle.bind(this)
+		this.handle = this.handle.bind(this)
 	}
 
 	hydrate(msg) {
 		msg.ws.selected_server_id = msg.payload.selected_server_id
 		msg.ws.selected_channel_id = msg.payload.selected_channel_id
-
 		console.log("[SELECTED SERVER][HYDRATE]: ", msg.ws.cache[0])
 	}
 
 
-	// handle(msg) {
-	// 	msg.ws.forEach(s => {
-
-	// 	})
-	// }
+	handle(msg) {
+		console.log("SELECTED SERVER: ", msg.payload)
+		this.hydrate(msg)
+	}
 }
 
 Selected_Server.EVENT = "UPDATE_SELECTED_SERVER"
